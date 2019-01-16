eff4d4cba3f0fb783e973630ccb2671b605756a1
Matthew Vilim <mvilim@stanford.edu>
2019-01-16 11:11:18 -0800

    i

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -4,13 +4,14 @@
 Keyboard keyboard;
 
 void setup(void) {
+  keyboard.begin();
 #if DEBUG
   Serial.begin(9600);
-  keyboard.begin();
 #endif
 }
 
 void loop(void) {
   keyboard.update();
+  // sleep to save power
   delay(10);
 }
